6a6702f393f0f8075b4b7fbe9ea6f01b74908a84
ModderAA <asherwizard@gmail.com>
2024-10-10 06:18:20 -0600

    Add files via upload

---
+++ src/main/java/net/asheranderson/mccourse/MCCourseModClient.java
@@ -0,0 +1,8 @@
+package net.asheranderson.mccourse;
+import net.fabricmc.api.ClientModInitializer;
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
